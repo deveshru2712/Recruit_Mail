8c97efbd388591caf34923a61f5dd5659cadaa73
Devesh Chandra <deveshru2712@gmail.com>
2025-06-27 13:46:26 +0530

    implement auth

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJs {
+  interface process {
+    NEXT_PUBLIC_AUTH_SECRET: string;
+
+    NEXT_PUBLIC_AUTH_GOOGLE_ID: string;
+    NEXT_PUBLIC_AUTH_GOOGLE_SECRET: string;
+  }
+}
